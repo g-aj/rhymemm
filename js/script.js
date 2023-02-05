999c84dfd8261c6403ebb43dba04607742c6450f
g-aj <112806947+g-aj@users.noreply.github.com>
2023-02-05 10:32:23 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$.scrollify({
+  section : ".box",//1ページスクロールさせたいエリアクラス名
+  scrollbars:"false",//スクロールバー表示・非表示設定
+  interstitialSection : "#header,#footer",//ヘッダーフッターを認識し、1ページスクロールさせず表示されるように設定
+  easing: "swing", // 他にもlinearやeaseOutExpoといったjQueryのeasing指定可能
+    scrollSpeed: 300, // スクロール時の速度
+
+  //以下、ページネーション設定
+  before:function(i,panels) {
+    var ref = panels[i].attr("data-section-name");
+      $(".pagination .active").removeClass("active");
+      $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+    },
+    afterRender:function() {
+      var pagination = "<ul class=\"pagination\">";
+      var activeClass = "";
+      $(".box").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
+        activeClass = "";
+        if(i===$.scrollify.currentIndex()) {
+          activeClass = "active";
+        }
+        pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+      });
+      pagination += "</ul>";
+
+      $("#box1").append(pagination);//はじめのエリアにページネーションを表示
+      $(".pagination a").on("click",$.scrollify.move);
+    }
+
+  });
+particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#786464"},"shape":{"type":"circle","stroke":{"width":0,"color":"#645151"},"polygon":{"nb_sides":10},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.30464829156444934,"random":false,"anim":{"enable":true,"speed":1.0557003759917487,"opacity_min":0.08932849335314796,"sync":false}},"size":{"value":12.02559045649142,"random":true,"anim":{"enable":false,"speed":63.34202255950492,"size_min":6.496617698410762,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":11.22388442605866,"direction":"none","random":false,"straight":true,"out_mode":"out","bounce":false,"attract":{"enable":true,"rotateX":1443.0708547789707,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
